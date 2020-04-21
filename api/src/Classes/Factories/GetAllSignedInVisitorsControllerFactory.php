e92bd1ecd0fd4616a114d65923e8dcc2027cefbe
robings <r_gordon_stables@hotmail.com>
2020-04-21 12:00:23 +0100

    added some invoke logic for GetAllSignedInVisitorsController

--- api/src/Classes/Factories/GetAllSignedInVisitorsControllerFactory.php
+++ api/src/Classes/Factories/GetAllSignedInVisitorsControllerFactory.php
@@ -3,9 +3,13 @@
 namespace SignInApp\Factories;
 
 use SignInApp\Controllers\GetAllSignedInVisitorsController;
+use Psr\Container\ContainerInterface;
 
 
 class GetAllSignedInVisitorsControllerFactory
 {
-
+    public function __invoke(ContainerInterface $container) : GetAllSignedInVisitorsController
+    {
+        $visitorsModel = $container->get('VisitorsModel');
+    }
 }

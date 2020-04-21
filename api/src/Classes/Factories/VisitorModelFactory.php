cbc73a193d1710a81572e8529562b112239745a7
chris tregaskis <chrisProjects@hanans-mbp.home>
2020-04-21 15:42:44 +0100

    returned file that got deleted

---
+++ api/src/Classes/Factories/VisitorModelFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace SignInApp\Factories;
+
+use Psr\Container\ContainerInterface;
+use SignInApp\Models\VisitorModel;
+
+class VisitorModelFactory
+{
+    /**
+     * Creates visitors model with dependencies.
+     *
+     * @param ContainerInterface $container
+     *
+     * @return VisitorModel returns object with db connection injected.
+     */
+    public function __invoke(ContainerInterface $container)
+    {
+        $db = $container->get('dbConnection');
+        return new VisitorModel($db);
+    }
+}

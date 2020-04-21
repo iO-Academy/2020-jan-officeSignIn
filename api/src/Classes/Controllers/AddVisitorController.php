ce33fadd233035e45058cfba9279816fe9813df9
chris tregaskis <chrisProjects@hanans-mbp.home>
2020-04-21 14:17:38 +0100

    completed draft setup for controller, needs testing

--- api/src/Classes/Controllers/AddVisitorController.php
+++ api/src/Classes/Controllers/AddVisitorController.php
@@ -23,10 +23,35 @@ class AddVisitorController
     public function __invoke(Request $request, Response $response, array $args)
     {
         $requestData = $request->getParsedBody();
+        $name = $requestData['Name'];
+        $company = $requestData['Company'];
         $responseData = [
             'Success' => false,
             'Message' => 'Error',
             'Data' => []
         ];
+        $statusCode = http_response_code();
+
+        if ($_SESSION['loggedIn'] === true) {
+            if (isset($requestData['Name']) && strlen($requestData['Name']) > 0) {
+                $successfulInsert = $this->visitorsModel->addVisitor($name, $company);
+                if ($successfulInsert) {
+                    $responseData = [
+                        'Success' => true,
+                        'Message' => 'Visitor successfully logged'
+                    ];
+                } elseif ($successfulInsert) {
+                    $responseData = [
+                        'Message' => 'Unable to connect to server'
+                    ];
+                } else {
+                    $responseData =[
+                        'Message' => 'Name is required'
+                    ];
+                    $statusCode = 400;
+                }
+            }
+            return $response->withJson($responseData, $statusCode);
+        }
     }
 }

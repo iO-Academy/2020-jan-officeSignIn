50d7b3aeed6e3f641cc038928e082c8a0c2ffd34
chris tregaskis <chrisProjects@hanans-mbp.home>
2020-04-21 13:43:05 +0100

    created add visitor controller factory

---
+++ api/src/Classes/Factories/AddVisitorControllerFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace SignInApp\Factories;
+
+use Psr\Container\ContainerInterface;
+use SignInApp\Controllers\AddVisitorController;
+
+class AddVisitorControllerFactory
+{
+    /**
+     * Instantiates a new AddVisitorController and injects a StageModel
+     *
+     * @param ContainerInterface $container
+     * @return AddVisitorController
+     */
+    public function __invoke(ContainerInterface $container)
+    {
+        $visitorsModel = $container->get('VisitorsModel');
+        return new AddVisitorController($visitorsModel);
+    }
+}

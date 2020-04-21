e20ecd212ee6121f8515cd1eb7de3b27aaf6abc9
paulms1 <stevenson.paulmichael@gmail.com>
2020-04-21 13:32:05 +0100

    began creating fetch request method on SignInForm.js

--- app/src/Components/LandingPage/MainContainer/SignInForm/SigninForm.js
+++ app/src/Components/LandingPage/MainContainer/SignInForm/SigninForm.js
@@ -4,12 +4,26 @@ import './SigninForm.css';
 class SigninForm extends React.Component {
     render() {
         return (
-            <form action="POST">
-                <input type="text" placeholder="Your first name and surname..."/>
-                <input type="text" placeholder="Your organisation..."/>
-                <input class="signInButton" type="submit" value="Sign In"/>
+            <form id="visitorSignInForm" action="POST">
+                <input id="name" type="text" placeholder="Your first name and surname..."/>
+                <input id="organisation" type="text" placeholder="Your organisation..."/>
+                <input className="signInButton" type="submit" value="Sign In"/>
             </form>
         )
+    }
+
+    postSignInData() {
+        let signInButton = document.querySelector('.signInButton');
+        signInButton.addEventListener('submit', (e)=>{
+            e.preventDefault();
+        });
+        let name = document.getElementById('name').value;
+        let organisation = document.getElementById('organisation').value;
+        const visitorDataToSend = {
+            method: 'POST', 
+            headers: { 'Content-Type': 'application/json' }, 
+            body: JSON.stringify({ name: name, organisation: organisation })
+        }
     }
 }
 

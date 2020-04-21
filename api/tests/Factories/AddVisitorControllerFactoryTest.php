02f8093e7eeaa8fefafa39238c201894a0cf1bf1
chris tregaskis <chrisProjects@hanans-mbp.home>
2020-04-21 15:48:25 +0100

    created test for instantiation of new add visitor controller factory

---
+++ api/tests/Factories/AddVisitorControllerFactoryTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Test\Factories;
+
+use PHPUnit\Framework\TestCase;
+use SignInApp\Factories\AddVisitorControllerFactory;
+
+class AddVisitorControllerFactoryTest extends TestCase
+{
+    /**
+     * checks to make sure factory class instantiated when calling new factory
+     */
+    public function testSuccessInvoke()
+    {
+        $factory = new AddVisitorControllerFactory();
+        $expected = AddVisitorControllerFactory::class;
+        $this->assertInstanceOf($expected, $factory);
+    }
+}

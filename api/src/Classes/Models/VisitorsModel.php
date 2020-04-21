58b7652df5556dd4d197bac22d688b02164f2379
chris tregaskis <chrisProjects@hanans-mbp.home>
2020-04-21 14:17:09 +0100

    created addVisitor method

--- api/src/Classes/Models/VisitorsModel.php
+++ api/src/Classes/Models/VisitorsModel.php
@@ -2,6 +2,8 @@
 
 namespace SignInApp\Models;
 
+use phpDocumentor\Reflection\Types\Boolean;
+
 class VisitorsModel
 {
     private $db;
@@ -14,5 +16,20 @@ class VisitorsModel
     public function __construct(\PDO $db)
     {
         $this->db = $db;
+    }
+
+    /**
+     *  Adds new visitor to the database and returns a bool based on success or failure
+     *
+     * @param $Name
+     * @param $Company
+     * @return bool
+     */
+    public function addVisitor($Name, $Company) : bool
+    {
+        $query = $this->db->prepare("INSERT INTO `visitors` (`Name`, `Company`) VALUES (:Name, :Company);");
+        $query->bindParam(':Name', $Name);
+        $query->bindParam(':Company', $Company);
+        return $query->execute();
     }
 }

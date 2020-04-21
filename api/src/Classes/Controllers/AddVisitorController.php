44d8a9e705b1cce58573a8e6b0f3dcfda7134a06
chris tregaskis <chrisProjects@hanans-mbp.home>
2020-04-21 15:33:15 +0100

    successfully added date and time to be sent in. Controller ready to be unit tested

--- api/src/Classes/Controllers/AddVisitorController.php
+++ api/src/Classes/Controllers/AddVisitorController.php
@@ -20,38 +20,53 @@ class AddVisitorController
         $this->visitorsModel = $visitorsModel;
     }
 
+    /**
+     *  On invoke check input for Name value, if data there, call addVisitor method
+     *  from VisitorsModel and response whether that was successful or not
+     *
+     * @param Request $request
+     * @param Response $response
+     * @param array $args
+     * @return Response
+     */
     public function __invoke(Request $request, Response $response, array $args)
     {
+        date_default_timezone_set("London (United Kingdom - England)");
+        $h = date('h') + 1;
         $requestData = $request->getParsedBody();
         $name = $requestData['Name'];
         $company = $requestData['Company'];
+        $dateOfVisit = date("Y-m-d");
+        $timeOfSignIn = date($h . ':i:s');
+        $signedIn = 1;
         $responseData = [
             'Success' => false,
             'Message' => 'Error',
             'Data' => []
         ];
-        $statusCode = http_response_code();
+        $statusCode = 500;
 
-        if ($_SESSION['loggedIn'] === true) {
-            if (isset($requestData['Name']) && strlen($requestData['Name']) > 0) {
-                $successfulInsert = $this->visitorsModel->addVisitor($name, $company);
-                if ($successfulInsert) {
-                    $responseData = [
-                        'Success' => true,
-                        'Message' => 'Visitor successfully logged'
-                    ];
-                } elseif ($successfulInsert) {
-                    $responseData = [
-                        'Message' => 'Unable to connect to server'
-                    ];
-                } else {
-                    $responseData =[
-                        'Message' => 'Name is required'
-                    ];
-                    $statusCode = 400;
-                }
+        if (isset($requestData['Name']) && strlen($requestData['Name']) > 0) {
+            $successfulInsert = $this->visitorsModel->addVisitor($name, $company, $dateOfVisit, $timeOfSignIn, $signedIn);
+            if ($successfulInsert) {
+                $responseData = [
+                    'Success' => true,
+                    'Message' => 'Visitor successfully logged'
+                ];
+                $statusCode = 200;
+            } else {
+                $responseData = [
+                    'Success' => false,
+                    'Message' => 'Unable to connect to server'
+                ];
             }
-            return $response->withJson($responseData, $statusCode);
+        } else {
+            $responseData =[
+                'Success' => false,
+                'Message' => 'Name is required'
+            ];
+            $statusCode = 400;
         }
+        return $response->withJson($responseData, $statusCode);
     }
 }

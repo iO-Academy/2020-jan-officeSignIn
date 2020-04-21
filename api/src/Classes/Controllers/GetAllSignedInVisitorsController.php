9552136578b292e4e2790b887e3937322a34bdd2
robings <r_gordon_stables@hotmail.com>
2020-04-21 11:55:19 +0100

    added logic to return a response to the GetAllSignedInVisitors Controller

--- api/src/Classes/Controllers/GetAllSignedInVisitorsController.php
+++ api/src/Classes/Controllers/GetAllSignedInVisitorsController.php
@@ -10,8 +10,26 @@ class GetAllSignedInVisitorsController
 {
     private $visitorsModel;
 
-    public function __construct()
+    /**
+     * GetAllSignedInVisitorsController constructor.
+     *
+     * @param $visitorsModel
+     */
+    public function __construct(VisitorsModel $visitorsModel)
     {
+        $this->visitorsModel = $visitorsModel;
+    }
 
+    public function __invoke(Request $request, Response $response, array $args)
+    {
+        //once tested, surround this with an if statement to check admin logged in status
+        $apiResponse = [
+            'Success' => false,
+            'Message' => 'Unable to retrieve data',
+            'Data' => []
+        ];
+
+        $apiResponse['Data'] = $this->visitorsModel->getAllSignedInVisitors();
+        return $response->withJson($apiResponse);
     }
 }

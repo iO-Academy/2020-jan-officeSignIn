a005196f5e89ab2cc40abf6408a5f2169dfb431e
robings <r_gordon_stables@hotmail.com>
2020-04-21 21:53:13 +0100

    added invoke test for GetAllSignedInVisitorsControllerFactoryTest

--- api/tests/Factories/GetAllSignedInVisitorsControllerFactoryTest.php
+++ api/tests/Factories/GetAllSignedInVisitorsControllerFactoryTest.php
@@ -4,16 +4,32 @@ namespace Test\Factories;
 
 use PHPUnit\Framework\TestCase;
 use SignInApp\Factories\GetAllSignedInVisitorsControllerFactory;
+use SignInApp\Controllers\GetAllSignedInVisitorsController;
+use SignInApp\Models\VisitorModel;
+use Psr\Container\ContainerInterface;
 
 class GetAllSignedInVisitorsControllerFactoryTest extends TestCase
 {
     /**
      * checks to make sure factory class instantiated when calling new factory
      */
-    public function testSuccessInstantiation()
+    public function testInstantiation()
     {
         $factory = new GetAllSignedInVisitorsControllerFactory();
         $expected = GetAllSignedInVisitorsControllerFactory::class;
         $this->assertInstanceOf($expected, $factory);
+    }
+
+    public function testInvoke()
+    {
+        $container = $this->createMock(ContainerInterface::class);
+        $visitorModel = $this->createMock(VisitorModel::class);
+        $container->method('get')
+            ->willReturn($visitorModel);
+
+        $factory = new GetAllSignedInVisitorsControllerFactory();
+        $case = $factory($container);
+        $expected = GetAllSignedInVisitorsController::class;
+        $this->assertInstanceOf($expected, $case);
     }
 }

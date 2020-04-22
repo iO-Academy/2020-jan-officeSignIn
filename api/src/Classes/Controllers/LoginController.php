a4014cb41db3faeeb0c1f4642bb8e35347be5cdb
robings <r_gordon_stables@hotmail.com>
2020-04-22 19:56:19 +0100

    added function to check passcode

--- api/src/Classes/Controllers/LoginController.php
+++ api/src/Classes/Controllers/LoginController.php
@@ -12,4 +12,33 @@ class LoginController
     {
         $this->adminModel = $adminModel;
     }
+
+    public function __invoke($request, $response, $args)
+    {
+        $usersPasscode = $request->getParsedBodyParam('Passcode');
+        $hashedPasscode = $this->adminModel->getHashedPasscode();
+
+        if(password_verify($usersPasscode, $hashedPasscode['Passcode'])) {
+
+            //move key to seperate file
+            $key = "super_secret_key";
+            $expiryTimestamp = time() + 60;
+            $payload = [
+                "exp" => $expiryTimestamp,
+                "iat" => time()
+            ];
+            $jwt = JWT::encode($payload, $key);
+            $data = [
+                "success" => true,
+                "token" => $jwt
+            ];
+            return $response->withJson($data);
+        } else {
+            $data = [
+                "success" => false,
+                "message" => "credentials incorrect"
+            ];
+            return $response->withJson($data);
+        }
+    }
 }

304a682747d56fa176a0e1f9b34e2f821f1d9cf1
paulms1 <stevenson.paulmichael@gmail.com>
2020-04-22 11:39:35 +0100

    enabled cors for localhost:3000

--- api/src/routes.php
+++ api/src/routes.php
@@ -5,8 +5,27 @@ use Slim\Http\Request;
 use Slim\Http\Response;
 
 return function (App $app) {
+    $app->options('/{routes:.+}', function ($request, $response, $args) {
+        return $response;
+    });
+
+    $app->add(function ($req, $res, $next) {
+        $response = $next($req, $res);
+        return $response
+            ->withHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
+            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    });
 
     //Api Routes
     $app->post('/api/visitorSignIn', 'AddVisitorController');
 
+    // Catch-all route to serve a 404 Not Found page if none of the routes match
+    // NOTE: make sure this route is defined last
+    $app->map(['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], '/{routes:.+}', function($req, $res) {
+        $handler = $this->notFoundHandler; // handle using the default Slim page not found handler
+        return $handler($req, $res);
+    });
 };
+
+

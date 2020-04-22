a9edb63d6cf711ca44a72ffeafc64793f2ef01ca
robings <r_gordon_stables@hotmail.com>
2020-04-22 21:02:36 +0100

    added doc blocks to functions

--- api/src/Classes/Controllers/LoginController.php
+++ api/src/Classes/Controllers/LoginController.php
@@ -2,23 +2,53 @@
 
 namespace SignInApp\Controllers;
 
+use Slim\Http\Request;
+use Slim\Http\Response;
 use SignInApp\Models\AdminModel;
+use SignInApp\Entities\ValidationEntity;
 use \Firebase\JWT\JWT;
 
-class LoginController
+class LoginController extends ValidationEntity
 {
     private $adminModel;
+
+    /**
+     * LoginController constructor.
+     *
+     * @param AdminModel $adminModel - the admin model passed in
+     */
     public function __construct(AdminModel $adminModel)
     {
         $this->adminModel = $adminModel;
     }
 
-    public function __invoke($request, $response, $args)
+    /**
+     * Compares entered passcode against hashed passcode in the database, returns a jwt key if successful
+     * or an error message if not
+     *
+     * @param $request
+     *
+     * @param $response
+     *
+     * @param $args
+     *
+     * @return object - the data to return to the requestor
+     */
+    public function __invoke(Request $request, Response $response, array $args) :object
     {
-        $usersPasscode = $request->getParsedBodyParam('Passcode');
+        $adminPasscode = $request->getParsedBodyParam('Passcode');
+
+        if (!self::checkFourDigitInput($adminPasscode)) {
+            $data = [
+                "success" => false,
+                "message" => "credentials incorrect"
+            ];
+            return $response->withJson($data);
+        }
+
         $hashedPasscode = $this->adminModel->getHashedPasscode();
 
-        if(password_verify($usersPasscode, $hashedPasscode['Passcode'])) {
+        if(password_verify($adminPasscode, $hashedPasscode[0]['passcode'])) {
 
             //move key to seperate file
             $key = "super_secret_key";

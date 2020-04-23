77214a50f7ecafc295657f608af604ee736f0601
robings <r_gordon_stables@hotmail.com>
2020-04-23 23:28:06 +0100

    added logic to push back to landing page

--- app/src/Components/AdminPage/AdminPage.js
+++ app/src/Components/AdminPage/AdminPage.js
@@ -3,7 +3,53 @@ import './adminPage.css';
 import AdminContainer from "./AdminContainer/AdminContainer";
 import LogOutBtn from "./LogOutBtn/LogOutBtn";
 
+// let loggedIn=false;
+//
+// if (localStorage.getItem('bearerToken') !== '') {
+//     loggedIn=true;
+// } else {
+//     loggedIn=false;
+// }
+// console.log(loggedIn)
+
+
+
 class AdminPage extends React.Component{
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            success: '',
+            bearerToken: localStorage.getItem('bearerToken')
+        };
+
+        console.log(this.state.bearerToken)
+    }
+
+    componentDidMount() {
+        this.fetchVisitors();
+    }
+
+    fetchVisitors = () => {
+        fetch('http://localhost:8080/api/admin', {
+            method: 'GET',
+            headers: {
+                "Content-Type" : "application/json",
+                "Authorization" : "Bearer " + this.state.bearerToken
+            }
+        })
+            .then(data=>data.json())
+            .then((data)=>{
+                this.setState({
+                    success: data.Success
+                })
+                if (!(this.state.success)) {
+                    window.location.replace('http://localhost:3000')
+                }
+            })
+
+    };
+
     render() {
         return (
             <div>

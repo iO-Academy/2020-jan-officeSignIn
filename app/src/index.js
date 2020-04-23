62862a22ab615e8e4f76003c307241c45f32037e
chris tregaskis <chrisProjects@hanans-mbp.home>
2020-04-23 14:42:08 +0100

    created routing

--- app/src/index.js
+++ app/src/index.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import LandingPage from "./Components/LandingPage/LandingPage";
+import AdminPage from "./Components/AdminPage/AdminPage";
+import { Route, Link, BrowserRouter as Router, Switch } from 'react-router-dom';
+
+class Routing extends React.Component {
+    render() {
+        return (
+            <Router>
+                <div>
+                    <Switch>
+                        <Route exact path="/" component={ LandingPage }/>
+                        <Route path="/AdminPage" component={ AdminPage }/>
+                        <Route component={ LandingPage }/>
+                    </Switch>
+                </div>
+            </Router>
+        );
+    }
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Routing />
   </React.StrictMode>,
   document.getElementById('root')
 );

8cf8b7415c4e8021e5a2c40b96a853c1bf8b2b2d
chris tregaskis <chrisProjects@hanans-mbp.home>
2020-04-23 12:55:06 +0100

    created logout button

---
+++ app/src/Components/AdminPage/LogOutBtn/LogOutBtn.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './logOutBtn.css';
+
+class LogOutBtn extends React.Component {
+    render() {
+        return (
+            <button>Log Out</button>
+        )
+    }
+}
+
+export default LogOutBtn;

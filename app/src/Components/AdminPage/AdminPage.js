55abae609d7879d4f3d8126a3bddc1f68c44b165
chris tregaskis <chrisProjects@hanans-mbp.home>
2020-04-23 12:51:15 +0100

    currently rendering out admin page, need to build route in index

--- app/src/Components/AdminPage/AdminPage.js
+++ app/src/Components/AdminPage/AdminPage.js
@@ -1,13 +1,14 @@
 import React from "react";
 import './adminPage.css';
 import VisitorsTable from "./VisitorsTable/VisitorsTable";
+import MainContainer from "../Components/LandingPage/MainContainer/MainContainer";
 
 class AdminPage extends React.Component{
     render() {
         return (
             <div>
                 <h1>All visitors currently signed in today:</h1>
-                <VisitorsTable/>
+                <MainContainer/>
             </div>
         );
     }

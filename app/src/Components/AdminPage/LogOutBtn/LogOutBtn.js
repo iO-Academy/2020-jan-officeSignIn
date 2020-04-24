1cce2994cc9820dec78521993e2bf42b4e9c95bc
daniellaskinner <daniellaskinner120@gmail.com>
2020-04-24 09:26:23 +0100

    started function to boot back to login page

--- app/src/Components/AdminPage/LogOutBtn/LogOutBtn.js
+++ app/src/Components/AdminPage/LogOutBtn/LogOutBtn.js
@@ -2,6 +2,13 @@ import React from "react";
 import './logOutBtn.css';
 
 class LogOutBtn extends React.Component {
+    logout(){
+        localStorage.clear();
+        return(
+            <Redirect to="/"/>
+        )
+    }
+
     render() {
         return (
             <button className="logOutBtn">Log Out</button>

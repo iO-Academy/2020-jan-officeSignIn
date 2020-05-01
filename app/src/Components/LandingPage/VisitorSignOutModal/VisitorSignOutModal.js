70056f9a3eaf3ca918957396962e5b345bc0fea2
robings <r_gordon_stables@hotmail.com>
2020-05-01 16:00:05 +0100

    added table and completed logic to show and hide the modal

--- app/src/Components/LandingPage/VisitorSignOutModal/VisitorSignOutModal.js
+++ app/src/Components/LandingPage/VisitorSignOutModal/VisitorSignOutModal.js
@@ -1,4 +1,6 @@
 import React from "react";
+import './VisitorSignOutModal.css'
+const columnHeader = ['Name', 'Time Signed In'];
 
 class VisitorSignOutModal extends React.Component
 {
@@ -7,6 +9,9 @@ class VisitorSignOutModal extends React.Component
 
         this.state = {
             modalClass: this.props.signOutModalVisible ? 'visible' : 'hidden',
+            visitorPackage: {
+                Data: []
+            },
             response: ''
         };
     }
@@ -25,13 +30,51 @@ class VisitorSignOutModal extends React.Component
         }
     }
 
+    generateRows = () => {
+        let result = [];
+        let tableData = this.state.visitorPackage.Data;
+
+        for(var i = 0; i < tableData.length; i++) {
+            let timeOfSignIn = tableData[i].TimeOfSignIn;
+            timeOfSignIn = timeOfSignIn.substring(0,5);
+            result.push(
+                <tr key={i} data-id={tableData[i].id}>
+                    <td key={tableData[i].Name}>{tableData[i].Name}</td>
+                    {/*<td key={tableData[i].Company}>{tableData[i].Company}</td>*/}
+                    <td key={timeOfSignIn}>{timeOfSignIn}</td>
+                </tr>
+            )
+
+        }
+        return result;
+    };
+
+    generateHeader = () => {
+        let result = [];
+        for(var i = 0; i < columnHeader.length; i++) {
+            result.push(<th key={columnHeader[i]}>{columnHeader[i]}</th>)
+        }
+        return result;
+    };
+
     render() {
         let visibleState = 'signOutModal ' + this.state.modalClass;
         console.log(visibleState)
         return (
             <div className={visibleState}>
                 <span className="instructions">When did you sign in?</span>
-
+                <div className="col-12 visitorsTable">
+                    <table className="table table-bordered table-hover">
+                        <thead>
+                        <tr>
+                            {this.generateHeader()}
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {this.generateRows()}
+                        </tbody>
+                    </table>
+                </div>
                 <button className="closeModalBtn" onClick={this.props.updateSignOutModalVisible}>X</button>
                 <div className="responseMessage">{this.state.response}</div>
             </div>

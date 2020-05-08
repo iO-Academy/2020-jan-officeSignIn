8d07ef7f61fdcaadf25ab3ebdc0600c319f04423
chris tregaskis <chrisProjects@hanans-mbp.home>
2020-05-08 18:18:29 +0100

    added functionality to toggle a success tick when visitor successfully sign in or out

--- app/src/Components/LandingPage/MainContainer/MainContainer.js
+++ app/src/Components/LandingPage/MainContainer/MainContainer.js
@@ -3,10 +3,13 @@ import './MainContainer.css';
 import Logo from './Logo/Logo';
 import SigninForm from "./SignInForm/SigninForm";
 import MessageBox from "./MessageBox/MessageBox";
+import {Spring} from "react-spring/renderprops-universal";
 
 class MainContainer extends React.Component {
     state = {
-        response: ''
+        response: '',
+        successTick: false,
+        successTickVisible: 'hiddenOpacity'
     };
 
     updateResponse = (newResponse) => {
@@ -20,15 +23,48 @@ class MainContainer extends React.Component {
         this.setState({response : ''})
     };
 
+    toggleSuccessTick = () => {
+        this.setState({successTick: !this.state.successTick})
+    };
+
+    setSuccessTickHidden = () => {
+        this.setState({successTickVisible: 'hiddenOpacity'})
+    };
+
+    componentDidUpdate(prevProps, prevState) {
+        if (prevState.successTick !== this.state.successTick) {
+            if (this.state.successTick) {
+                this.setState({successTickVisible: 'visible'})
+            } else {
+                this.setState({successTickVisible: 'hiddenOpacity'})
+            }
+        }
+    }
+
     render() {
+        const successTickClass = 'successTick ' + this.state.successTickVisible;
         return (
             <main>
                 <div className="mainContainer">
+                    {!this.state.successTick ? (<div> </div>) : (
+                        <Spring
+                            from={{marginTop: -1000}}
+                            to={{marginTop: 0}}
+                        >
+                            {props => (
+                                <div  className={successTickClass} style={props}>
+                                    &#10004;
+                                </div>
+                            )}
+                        </Spring>
+                    )}
                     <Logo/>
                     <SigninForm
                         updateResponse={this.updateResponse}
                         updateSignOutModalVisible={this.props.updateSignOutModalVisible}
                         getSignOutData={this.props.getSignOutData}
+                        toggleSuccessTick={this.toggleSuccessTick}
+                        setSuccessTickHidden={this.setSuccessTickHidden}
                     />
                     <div className="responseMessageFailed">
                         <MessageBox response={this.state.response}/>

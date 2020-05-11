01399c14757ab21e1ecef66f3726ae4d08ba9c72
chris tregaskis <chrisProjects@hanans-mbp.home>
2020-05-11 20:32:14 +0100

    removed dulicated semi colon

--- api/src/routes.php
+++ api/src/routes.php
@@ -39,7 +39,7 @@ return function (App $app) {
     $app->post('/api/visitorSignIn', 'AddVisitorController');
     $app->get('/api/admin', 'GetAllSignedInVisitorsController')->add('Authenticate');
     $app->put('/api/visitorSignOut', 'SignOutVisitorController');
-    $app->get('/api/signedOutVisitors', 'GetAllSignedOutVisitorsController')->add('Authenticate');;
+    $app->get('/api/signedOutVisitors', 'GetAllSignedOutVisitorsController')->add('Authenticate');
 
     //Route to acquire authenticated active token
     $app->post('/adminLogin', 'LoginController');

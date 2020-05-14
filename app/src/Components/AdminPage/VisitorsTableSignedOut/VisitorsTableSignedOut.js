eb678ea5d28bf3cbae8a65aa351a586d459d333c
chris tregaskis <chrisProjects@hanans-mbp.home>
2020-05-14 10:44:13 +0100

    created the initial draft of visitor signed out table component. Successfully displaying however header not working

---
+++ app/src/Components/AdminPage/VisitorsTableSignedOut/VisitorsTableSignedOut.js
@@ -0,0 +1,100 @@
+import React from "react";
+import '../VisitorsTableSignedIn/visitorsTable.css';
+const columnHeader = ['Name', 'Company', 'Date', 'Time In', 'Time Out'];
+
+
+class VisitorsTableSignedOut extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            visitorPackage: {
+                "Data": []
+            },
+            bearerToken: localStorage.getItem('bearerToken'),
+            appUrl: localStorage.getItem('appUrl')
+        };
+    }
+
+    componentDidMount() {
+        this.fetchVisitors();
+    }
+
+    fetchVisitors = () => {
+        const url = localStorage.getItem('apiUrl') + 'api/signedOutVisitors';
+        fetch(url, {
+            method: 'GET',
+            headers: {
+                "Content-Type" : "application/json",
+                "Authorization" : "Bearer " + this.state.bearerToken
+            }
+        })
+            .then(data=>data.json())
+            .then((data)=>{
+                if (data.message === null || data.message ==='Invalid token' ||
+                    data.message === 'Malformed token' || data.message === 'Token has expired' ||
+                    data.message === 'Token error' || data.message === 'No token received') {
+                    localStorage.removeItem('bearerToken');
+                    window.location.replace(this.state.appUrl)
+                }
+
+                this.setState({
+                    visitorPackage: data
+                });
+
+                if (!data.Data.length > 0) {
+                    this.props.updateResponse('No visitors currently signed Out');
+                }
+                localStorage.removeItem('bearerToken')
+            })
+    };
+
+    generateHeader = () => {
+        let result = [];
+        for(let i = 0; i < columnHeader.length; i++) {
+            result.push(<th key={columnHeader[i]}>{columnHeader[i]}</th>)
+        }
+        return result;
+    };
+
+    generateRows = () => {
+        let result = [];
+        let tableData = this.state.visitorPackage.Data;
+
+        for(let i = 0; i < tableData.length; i++) {
+            let timeOfSignIn = tableData[i].TimeOfSignIn;
+            let timeOfSignOut = tableData[i].TimeOfSignOut;
+            timeOfSignIn = timeOfSignIn.substring(0,5);
+            timeOfSignOut = timeOfSignOut.substring(0,5);
+            result.push(
+                <tr key={i} className="d-flex">
+                    <td key={tableData[i].Name} className="col-3">{tableData[i].Name}</td>
+                    <td key={tableData[i].Company} className="col-3">{tableData[i].Company}</td>
+                    <td key={tableData[i].Company} className="col-2">{tableData[i].DateOfVisit}</td>
+                    <td key={timeOfSignIn} className="col-2">{timeOfSignIn}</td>
+                    <td key={timeOfSignOut} className="col-2">{timeOfSignOut}</td>
+                </tr>
+            )
+        }
+        return result;
+    };
+
+    render() {
+        return (
+            <div className="col-12 visitorsTable">
+                <table className="table table-bordered table-hover">
+                    <thead>
+                    <tr>
+                        {/*{this.generateHeader()}*/}
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {this.generateRows()}
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+}
+
+export default VisitorsTableSignedOut;

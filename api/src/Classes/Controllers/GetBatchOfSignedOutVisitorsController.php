5950ff88a893b1862548cbc6919eedf33659cb3f
Christopher Tregaskis <christophertregaskis@christophersmbp.home>
2020-05-16 20:05:35 +0100

    initial draft of controller set up, successfully handling invalid input

---
+++ api/src/Classes/Controllers/GetBatchOfSignedOutVisitorsController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace SignInApp\Controllers;
+
+use SignInApp\Entities\ValidationEntity;
+use Slim\Http\Request;
+use Slim\Http\Response;
+use SignInApp\Models\VisitorModel;
+
+class GetBatchOfSignedOutVisitorsController extends ValidationEntity
+{
+    private $visitorModel;
+
+    /**
+     * GetBatchOfSignedOutVisitorsController constructor.
+     *
+     * @param VisitorModel $visitorModel
+     */
+    public function __construct(VisitorModel $visitorModel)
+    {
+        $this->visitorModel = $visitorModel;
+    }
+
+    public function __invoke(Request $request, Response $response, array $args)
+    {
+        $count = $_GET['count'];
+        $start = $_GET['start'];
+        $responseData = [
+            'Success' => false,
+            'Message' => 'Unable to connect to server',
+            'Data' => []
+        ];
+        $statusCode = 500;
+
+        if (strlen($count) < 1 || strlen($start) < 1 ||
+            self::checkDigitInput($count) === false || self::checkDigitInput($start) === false) {
+            $responseData = [
+                'Success' => false,
+                'Message' => 'Count and Start can not be empty and must be a number'
+            ];
+            $statusCode = 400;
+
+        }
+
+        var_dump($count);
+        var_dump($start);
+
+        return $response->withJson($responseData, $statusCode);
+
+
+
+    }
+}
